6a02b52e43a061338d1c10fa104e1c724b8e2099
Manuel Segovia <msegovia@mac.com>
2021-04-08 15:17:45 -0400

    correcting cors

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const createServer = async (config) => {
 		host,
 		port,
 		routes: {
-			cors: true,
-			origin: ['http://127.0.0.1:5500'],
+			//cors: true,
+			origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
 			validate: {
 				failAction: async (request, h, error) =>
 					error.isJoi
