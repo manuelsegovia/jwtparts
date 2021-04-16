9a59c8e8e596a8a66a4c92f520653c7722fe813f
Manuel Segovia <msegovia@mac.com>
2021-04-15 21:25:38 -0400

    added upload and conversion csv to json

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,11 @@ const {
 	addPartController,
 	getPartsController,
 } = require('../controllers/partsControllers');
+const csvjson = require('csvjson');
+const fs = require('fs');
 
 const { partSchema } = require('../schemas/partSchema');
+const { steelPlateWeight } = require('../helpers/weightCalc');
 
 const getParts = {
 	method: 'GET',
@@ -20,5 +23,51 @@ const newPart = {
 	},
 	handler: addPartController,
 };
+const massUpload = {
+	method: 'POST',
+	path: '/upload',
+	options: {
+		payload: {
+			output: 'data',
+			parse: true,
+			multipart: true,
+			maxBytes: 10 * 1024 * 1024,
+		},
+		description: 'mass upload',
+	},
+	handler: async (request, h) => {
+		const uploadFile = request.payload.csvFile;
+		const filejson = csvjson.toObject(uploadFile);
 
-module.exports = [getParts, newPart];
+		const y = await filejson.map((part) => {
+			const weightLbs = steelPlateWeight(
+				part.thicknessIn,
+				part.widthIn,
+				part.lengthIn
+			);
+			part.weightLbs = weightLbs;
+			return part;
+		});
+
+		try {
+			const insertedParts = await request.mongo.db
+				.collection('partsMaster')
+				.insertMany(y, { ordered: false });
+
+			return insertedParts;
+		} catch (error) {
+			const duplicated = error.writeErrors.map((element) => {
+				return element.err.op.sku;
+			});
+			fs.writeFile(`./temp/duplicated.txt`, duplicated.toString(), (err) => {
+				if (err) {
+					console.log('ERRTEMP', err);
+					return;
+				}
+			});
+			return h.response({ Invalid: duplicated });
+		}
+	},
+};
+
+module.exports = [getParts, newPart, massUpload];
